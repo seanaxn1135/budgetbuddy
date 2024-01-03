fed4e9932b5985f1946e2143be999776814f21fc
seanaxn1135 <seanaxn1135@gmail.com>
2024-01-03 21:20:43 +0800

    Add test for text-handler

--- src/commands/__tests__/text-handler.spec.ts
+++ src/commands/__tests__/text-handler.spec.ts
@@ -6,14 +6,25 @@ describe('Text Handler', () => {
   const createCtxWithMessage = (text: string): BotContext =>
     ({
       message: {
-        from: { id: 1 },
-        date: 123456,
         text,
       },
       reply: jest.fn(),
     }) as unknown as BotContext
+
+  it('should identify income', async () => {
+    ctx = createCtxWithMessage('+1000 Salary')
+    await textHandler(ctx)
+    expect(ctx.reply).toHaveBeenCalledWith('This is income')
+  })
+
+  it('should identify expense', async () => {
+    ctx = createCtxWithMessage('2000 Rent')
+    await textHandler(ctx)
+    expect(ctx.reply).toHaveBeenCalledWith('This is expense')
+  })
+
   it('should reply "I do not understand" if text is not valid', async () => {
-    ctx = createCtxWithMessage('SomeText')
+    ctx = createCtxWithMessage('Invalid Text')
     await textHandler(ctx)
     expect(ctx.reply).toHaveBeenCalledWith('I do not understand')
   })
