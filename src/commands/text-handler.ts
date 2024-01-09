252fa4a8aeb026941e2017dea8c9d26241945412
seanaxn1135 <seanaxn1135@gmail.com>
2024-01-09 23:30:48 +0800

    Refactor text-handler

--- src/commands/text-handler.ts
+++ src/commands/text-handler.ts
@@ -3,14 +3,16 @@ import type { BotContext } from '../global'
 import { isValidAmount } from '../helpers/is-valid-amount'
 import { INVALID_FORMAT_MESSAGE } from '../constants/messages'
 
+const SCENES = {
+  CATEGORIZE_INCOME: 'CATEGORIZE_INCOME',
+  CATEGORIZE_EXPENSE: 'CATEGORIZE_EXPENSE',
+}
+
 const textHandler = async (ctx: BotContext): Promise<void> => {
   ctx.session = {}
-  const message = deunionize(ctx.message)
-  if (message === null || message === undefined) {
-    return
-  }
 
-  if (ctx.from === undefined) {
+  const message = deunionize(ctx.message)
+  if (message?.text == null || ctx.from == null) {
     return
   }
 
@@ -19,17 +21,27 @@ const textHandler = async (ctx: BotContext): Promise<void> => {
     return
   }
 
-  if (isIncome(text)) {
-    const transactionDetails = parseTransactionDetails(text)
-    ctx.session.transaction = { ...transactionDetails }
-    await ctx.scene.enter('CATEGORIZE_INCOME')
-  } else if (isExpense(text)) {
-    const transactionDetails = parseTransactionDetails(text)
-    ctx.session.transaction = { ...transactionDetails }
-    await ctx.scene.enter('CATEGORIZE_EXPENSE')
-  } else {
-    await ctx.reply(INVALID_FORMAT_MESSAGE)
+  try {
+    if (isIncome(text)) {
+      await processTransaction(ctx, text, SCENES.CATEGORIZE_INCOME)
+    } else if (isExpense(text)) {
+      await processTransaction(ctx, text, SCENES.CATEGORIZE_EXPENSE)
+    } else {
+      await ctx.reply(INVALID_FORMAT_MESSAGE)
+    }
+  } catch (error) {
+    console.error('Error processing text:', error)
   }
+}
+
+const processTransaction = async (
+  ctx: BotContext,
+  text: string,
+  command: string
+): Promise<void> => {
+  const transactionDetails = parseTransactionDetails(text)
+  ctx.session.transaction = { ...transactionDetails }
+  await ctx.scene.enter(command)
 }
 
 const extractAmount = (text: string): string => text.split(' ')[0]
@@ -46,12 +58,13 @@ const isExpense = (text: string): boolean => {
 
 const parseTransactionDetails = (
   input: string
-): {
-  amount: number
-  description: string
-} => {
+): { amount: number; description: string } => {
   const detailsArray = input.trim().split(' ')
-  const amount = parseFloat(detailsArray[0].replace(/[^\d.]/g, ''))
+  const amount = parseFloat(detailsArray[0].replace(/[^\d.-]/g, ''))
+
+  if (isNaN(amount)) {
+    throw new Error('Invalid amount')
+  }
 
   const description = detailsArray.slice(1).join(' ')
   return { amount, description }
